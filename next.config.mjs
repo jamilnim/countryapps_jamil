2472891bc0ecd783ac9ce14a1a36f87ffe471d57
jamilnim <jamilnimbook2@gmail.com>
2025-12-24 22:27:23 +0200

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "openweathermap.org",
-          },
-          {
-            protocol: "https",
-            hostname: "lh3.googleusercontent.com",
-            port: "",
-            pathname: "./**",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "./**",
+      },
+    ],
+  },
+  turbopack: {
+    root: process.cwd(), // ✅ use this in ESM
+  },
 };
 
 export default nextConfig;
